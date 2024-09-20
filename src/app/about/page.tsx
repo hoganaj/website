7dc8d3f944d9a12be3b604ee1a2fc24d4cfb8cb6
Aidan Hogan <82148170+hoganaj@users.noreply.github.com>
2024-09-20 15:42:52 +0100

    duolingo streak widget

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,24 @@
+import DuoStreak from '@/components/duoStreak';
 import Timeline from '@/components/timeline';
 import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div className="container mx-auto p-4 flex flex-col items-center">
-      <div className="max-w-2xl w-full text-justify mb-8">
-        <p>
-          Lucky you! You have found my corner of the internet! This is an informal place to introduce myself, showcase my career experience, projects and anything that piques my interest worthy of sharing.
-        </p>
+    <>
+      <div className="container mx-auto p-4 flex flex-col items-center">
+        <div className="max-w-2xl w-full text-justify mb-8">
+          <p>
+            Lucky you! You have found my corner of the internet! This is an informal place to introduce myself, showcase my career experience, projects and anything that piques my interest worthy of sharing.
+          </p>
+        </div>
+        <div className="w-full flex justify-center">
+          <Timeline />
+        </div>
+        <div className="w-full flex justify-center mt-8">
+          <DuoStreak />
+        </div>
       </div>
-      <div className="w-full flex justify-center">
-        <Timeline />
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/duoStreak.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Image from "next/image";
+
+
+const DuoStreak: React.FC = () => {
+  async function getDuolingoStreak(username: string): Promise<any>{
+    try {
+      const response = await fetch(`https://www.duolingo.com/2017-06-30/users?username=${username}`)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      const streak = data.users[0].streak;
+      return streak
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  }
+
+  return (
+    <div className="inline-block bg-base-200 rounded-box shadow-md p-4">
+      <div className="flex items-center">
+        <div className="w-16 h-16 mr-4 flex-shrink-0">
+          <Image src="/duo.svg" alt="duo" width={200} height={200} className="w-full h-full object-cover" />
+        </div>
+        <div>
+          <div className="text-sm font-medium text-gray-500">Current Streak 🔥</div>
+          <div className="text-2xl font-bold flex items-center">
+            {getDuolingoStreak("aidsmcg")}
+            <Image src="https://ardslot.com/s/zs.svg" alt="duo" width={40} height={40} className="ml-2" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default DuoStreak;
