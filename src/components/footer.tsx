d233325c5672bae86586fd83606e936d80a17354
Aidan Hogan <82148170+hoganaj@users.noreply.github.com>
2024-09-21 00:03:18 +0100

    social links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,10 +9,10 @@ const Footer = () => {
       </div>
       <nav className="flex flex-wrap items-center gap-4">
         <h6 className="footer-title hover:text-neutral-950 m-0">中文</h6>
-        <a aria-label="Github" className="flex items-center">
+        <a aria-label="Github" href="https://github.com/hoganaj" target="_blank" className="flex items-center">
           <FaGithub size={24} className="fill-current hover:fill-neutral-950" />
         </a>
-        <a aria-label="Linkedin" className="flex items-center">
+        <a aria-label="Linkedin" href="https://www.linkedin.com/in/aidan-hogan-7b378a177/" target="_blank" className="flex items-center">
           <FaLinkedin size={24} className="fill-current hover:fill-blue-600" />
         </a>
       </nav>
