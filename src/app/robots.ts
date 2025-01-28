3218c67b6c44054fdff2880cd0f46fe20f7cab1b
Aidan Hogan <82148170+hoganaj@users.noreply.github.com>
2025-01-28 11:01:33 +0000

    robots

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  const url = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000';
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/studio',
+    },
+    sitemap: `${url}/sitemap.xml`,
+  }
+}
