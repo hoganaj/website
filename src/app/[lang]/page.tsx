17557526b04c2726ee73fce5ee471098e076831b
Aidan Hogan <82148170+hoganaj@users.noreply.github.com>
2025-01-31 00:23:56 +0000

    Home page info ui update

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
-import { FaLaptopCode, FaMapMarkerAlt, FaTools, FaSmile } from "react-icons/fa";
+import { FaLaptopCode, FaMapMarkerAlt, FaTools, FaTableTennis } from "react-icons/fa";
 import { getDictionary } from "./dictionaries";
 import type { Metadata } from 'next';
 
@@ -46,23 +46,23 @@ export default async function Home({
         </div>
 
         {/* Mobile-Friendly Section */}
-        <div className="mt-8 space-y-4">
-          <div className="flex items-center justify-center space-x-2">
-            <FaLaptopCode className="text-blue-500 text-xl" />
-            <p className="text-lg font-semibold">{dict.home.job}</p>
-          </div>
-          <div className="flex items-center justify-center space-x-2">
-            <FaMapMarkerAlt className="text-red-500 text-xl" />
-            <p className="text-lg">{dict.home.location}</p>
-          </div>
-          <div className="flex items-center justify-center space-x-2">
-            <FaTools className="text-green-500 text-xl" />
-            <p className="text-lg">{dict.home.skills}</p>
-          </div>
-          <div className="flex items-center justify-center space-x-2">
-            <FaSmile className="text-yellow-500 text-xl" />
-            <p className="text-lg">{dict.home.hobbies}</p>
-          </div>
+        <div className="mt-8 flex flex-col items-center gap-4">
+          {[
+            { icon: <FaLaptopCode />, text: dict.home.job, color: "text-blue-400" },
+            { icon: <FaMapMarkerAlt />, text: dict.home.location, color: "text-red-400" },
+            { icon: <FaTools />, text: dict.home.stack, color: "text-green-400" },
+            { icon: <FaTableTennis />, text: dict.home.hobbies, color: "text-yellow-400" },
+          ].map(({ icon, text, color }, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-4 px-6 py-4 w-full max-w-lg 
+                        backdrop-blur-lg bg-white/10 border dark:bg-white/5 border-white/20 
+                        rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
+            >
+              <span className={`text-2xl flex-shrink-0 ${color}`}>{icon}</span>
+              <p className="text-lg text-left text-base-content">{text}</p>
+            </div>
+          ))}
         </div>
 
         {/* Full Code Snippet for Larger Devices
